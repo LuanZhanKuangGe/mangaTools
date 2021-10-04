15049e8fa06c51daa722460beb2c700f0b692642
coderLuan <zhoubo616819598@sina.com>
2021-10-04 18:45:25 +0800

    更新

--- mangaWebsite.py
+++ mangaWebsite.py
@@ -1,22 +1,38 @@
 import os
 import os.path
+from gooey import Gooey, GooeyParser
 
+@Gooey(
+    program_name='IwaraTools',
+    image_dir='./',
+    language="chinese")
 
-rootdir = "./"
-fo = open("manga.html", "w", encoding="utf-8")
+def main():
+    parser = GooeyParser(description='漫画收藏生成网页, 配合油猴脚本使用.')
+    parser.add_argument('input', help="选择目标文件夹", widget='DirChooser')
+    parser.add_argument('output', help="选择目标文件夹", widget='DirChooser')
+    return parser.parse_args()
 
-fo.write("<html>\n")
+if __name__ == '__main__':
+    args = main()
 
-fo.write('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />')
-fo.write("<body>\n")
+    input = args.input + r'\\'
+    output = args.output + r'\\manga.html'
 
-for files in os.listdir(rootdir):
-    if files.endswith(".zip") or files.endswith(".rar"):
-        fo.write("<p>"+files.replace(".zip","")+"</p>\n")
+    fo = open(output, "w", encoding="utf-8")
 
-fo.write("</body>\n")
-fo.write("</html>\n")
+    fo.write("<html>\n")
 
-fo.close()
+    fo.write('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />')
+    fo.write("<body>\n")
 
-input("done")
+    for files in os.listdir(input):
+        if files.endswith(".zip") or files.endswith(".rar"):
+            fo.write("<p>"+files.replace(".zip","")+"</p>\n")
+
+    fo.write("</body>\n")
+    fo.write("</html>\n")
+
+    fo.close()
+
+    print("完成, 请查看" + output )
